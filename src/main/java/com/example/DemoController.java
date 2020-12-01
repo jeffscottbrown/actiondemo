46839b30f48aabd5346873616b6ea6a182abd1d2
Jeff Scott Brown <brownj@objectcomputing.com>
2020-12-01 10:52:34 -0600

    Github Action

---
+++ src/main/java/com/example/DemoController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+
+@Controller("/demo")
+public class DemoController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+
+    @Get(uri="/hello/{name}", produces = MediaType.TEXT_PLAIN)
+    public String greet(String name) {
+        return "Hello There " + name;
+    }
+}
