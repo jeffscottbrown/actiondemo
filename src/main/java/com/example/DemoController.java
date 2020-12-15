7fd043b51ed05eac89b5f109d366396adcbbcbb6
Jeff Scott Brown <brownj@objectcomputing.com>
2020-12-15 08:29:41 -0600

    update response

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @Get(uri="/", produces="text/plain")
     public String index() {
-        return "Response 6";
+        return "Response 7";
     }
 
     @Get(uri="/hello/{name}", produces = MediaType.TEXT_PLAIN)
